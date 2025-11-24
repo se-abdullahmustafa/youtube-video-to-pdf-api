ae9fba7ef0fff6bb60f31512f7d542c0170f2be9
Abdullah Mustafa <abdullahmustafa3607@gmail.com>
2025-11-25 00:15:51 +0500

    Update fastapi_app.py

--- fastapi_app.py
+++ fastapi_app.py
@@ -132,7 +132,7 @@ async def convert_video_to_pdf(
     time: str = Query(..., description="Time interval in minutes")
 ):
     try:
-        yt = YouTube(youtube_url, use_po_token=True, on_progress_callback=on_progress)
+        yt = YouTube(youtube_url)
         sanitized_video_id = sanitize_filename(yt.video_id)
         video_folder = f'video_{sanitized_video_id}'
         
